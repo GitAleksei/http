b43474c70582a3d9cb14e12f94a2362f95ba37fb
Kiva Aleksey <adolfp@mail.ru>
2021-06-09 16:52:24 +0000

    Init commit

---
+++ src/main/java/task1/Fact.java
@@ -0,0 +1,32 @@
+package task1;
+
+public class Fact {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private Integer upvotes;
+
+    public Fact(String id, String text, String type, String user, Integer upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
---
+++ src/main/java/task1/Main.java
@@ -0,0 +1,69 @@
+package task1;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static final String REMOTE_URI =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                    .setConnectTimeout(5000)
+                    .setSocketTimeout(30000)
+                    .setRedirectsEnabled(false)
+                    .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        String json = inputStreamToJson(response.getEntity().getContent());
+
+        List<Fact> facts = jsonToList(json);
+
+        List<Fact> voteFacts =  facts.stream()
+                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                .collect(Collectors.toList());
+
+        voteFacts.forEach(System.out::println);
+    }
+
+    private static List<Fact> jsonToList(String json) {
+        Type listType = new TypeToken<List<Fact>>(){}.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(json, listType);
+    }
+
+    private static String inputStreamToJson(InputStream inputStream) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String str;
+            while ((str = br.readLine()) != null) {
+                stringBuilder.append(str);
+            }
+            return stringBuilder.toString();
+        }
+    }
+}
